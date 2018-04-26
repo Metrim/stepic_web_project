4926fd0c12aedc740a24ccde6b9d9e748e616b4c
Unknown <pedchenko.sergey@gmail.com>
2018-04-26 21:17:24 +0400

    init2

--- init2.sh
+++ init2.sh
@@ -1,9 +1,3 @@
-echo 'Creating directories'
-mkdir -p web
-cd web && mkdir -p uploads public etc
-cd public && mkdir -p js css img && cd ..
-echo 'Create config files for nginx and unicorn'
-touch etc/nginx.conf && touch etc/gunicorn.conf
 echo 'Create links, remove default config from /etc/nginx/sites-enabled/default'
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default 2> /dev/null
