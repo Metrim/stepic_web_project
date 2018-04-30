c38b4399b5aefb9f4747896a3d92da69840aabd5
Unknown <pedchenko.sergey@gmail.com>
2018-04-30 12:06:13 +0400

    gunicorn

--- init.sh
+++ init.sh
@@ -3,6 +3,9 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default 2> /dev/null
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/test.conf
 sudo rm /etc/nginx/sites-available/default 2> /dev/null
+echo 'creare symbolic links to gunicorn configs'
+sudo -s ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
+#sudo -s ln -sf /home/box/web/etc/django-gunicorn.conf  /etc/gunicorn.d/django-gunicorn.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
