54f7157f90399cff2a12cdd6ad0eedd35dd4d441
Unknown <pedchenko.sergey@gmail.com>
2018-04-30 11:42:16 +0400

    conf

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 echo 'Create links, remove default config from /etc/nginx/sites-enabled/default'
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default 2> /dev/null
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/test.conf
+sudo rm /etc/nginx/sites-available/default 2> /dev/null
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
